d3a94cd09ddfc6f9772dfd137e609eb667749674
Jesus Juarez <jjayala1@gmail.com>
2020-12-02 23:16:17 -0600

    solutions day1 Part 1 and 2 V2

--- day1AB.py
+++ day1AB.py
@@ -5,7 +5,6 @@ data2 = data[1:]
 f.close()
 
 for a,b in [(a,b) for a in data for b in data2]:
-    t += 1
     if int(a)+int(b) == 2020:
         print(int(a)*int(b))
         break
@@ -15,10 +14,8 @@ for a,b in [(a,b) for a in data for b in data2]:
 f = open('day1.txt','r');
 data = f.readlines()
 f.close()
-t = 0
 
 for a,b,c in [(a,b,c) for a in data for b in data for c in data]:
-    t += 1
     if int(a)+int(b)+int(c) == 2020:
         print(int(a)*int(b)*int(c))
         break

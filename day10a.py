e8e416258da545b5a8eaac65edf80e67ecee639a
Jesus Juarez <jjayala1@gmail.com>
2020-12-17 15:29:16 -0600

    Day 10 optimization

--- day10a.py
+++ day10a.py
@@ -1,39 +1,19 @@
 def clean_data():
 
     f = open('day10.txt','r')
-    data = [int(x.replace('\n','')) for x in f.readlines()]
-    data.sort()
+    data = sorted([int(x.replace('\n','')) for x in f.readlines()])
+    data = [0, *data, data[-1] + 3 ]
     return data
 
-
 def sort_adapters(data):
-
-    dif1 = 1
-    dif3 = 1
-
-    for i in range(len(data)-1):
-
-        dif = data[i+1] - data[i]
-        #print(f'{i} -- {data[i+1]} - {data[i]} -- {data[i+1] - data[i]} -- {dif1} -- {dif3}')
-
-        if dif == 1:
-            dif1 += 1
-
-        elif dif == 3:
-            dif3 += 1
-
-    return dif1*dif3
+    dif = [(data[i+1]-data[i]) for i in range(len(data)-1) ]
+    return  dif.count(1) * dif.count(3)
 
 
 def count_arrangements(data):
 
-    new = [0]
-    new += data[:]
-    new.append(new[len(new)-1] + 3)
-    path1 = []
-
-    for i in range(len(new)-1):
-        path1.append(0)
+    new = data[:]
+    path1 = [0 for x in range(len(new)-1)]
     path1.append(1)
 
     for i in range(len(new)-1,-1,-1):
@@ -43,24 +23,19 @@ def count_arrangements(data):
             if i+j < len(new) and new[i+j] - new[i] <= 3:
                 paths += 1
                 path1[i] += path1[i+j]
-
         #print(f'Connector: {new[i]} -- Iteration: {i} -- Paths: {paths} -- TotPaths: {path1[i]} ')
     return path1[0]
 
+
 def count_arrangements1(data):
 
-    new = [0]
-    new += data[:]
-    new.append(new[len(new)-1] + 3)
-
-    path1 = []
-    path1.append(1)
+    new = data[:]
+    path1 = [0 for x in range(1,len(new))]
+    path1.insert(0,1)
 
     for i in range(len(new)):
 
-        path1.append(0)
         paths = 0
-
         for j in range(1,4):
             if i-j >= 0 and new[i] - new[i-j] <= 3:
                 paths += 1
